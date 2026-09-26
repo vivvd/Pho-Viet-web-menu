dd5491d3c9c3f75f3f38dcf54ba104d6029e4831
vivvd <viet2003.vn@gmail.com>
2026-09-26 22:22:36 +0300

    Travelpaouts integration test

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,25 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ru">
+      <head>
+        <script
+          {...{ nowprocket: '', 'seraph-accel-crit': '1' }}
+          data-noptimize="1"
+          data-cfasync="false"
+          data-wpfc-render="false"
+          data-no-defer="1"
+          data-cmp-ab="2"
+          dangerouslySetInnerHTML={{
+            __html: `(function () {
+  var script = document.createElement("script");
+  script.async = 1;
+  script.setAttribute("data-cmp-ab", "2");
+  script.src = 'https://tp-em.com/NTc4MzM2.js?t=578336';
+  document.head.appendChild(script);
+})();`,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
